b06c56b34362bdeb09972334d86efead23525667
Shlok-Bhakta <62256489+Shlok-Bhakta@users.noreply.github.com>
2020-11-09 19:53:24 -0600

    finished V1 map with random island rotation

---
+++ CoreWarUnityGame/Assets/islandrandomizer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class islandrandomizer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        randommovement();
+    }
+    void randommovement()
+    {
+       Quaternion randomMotiom = Quaternion.Euler(-90, 0, Random.Range(-180 , 180));
+
+        transform.rotation = randomMotiom;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
