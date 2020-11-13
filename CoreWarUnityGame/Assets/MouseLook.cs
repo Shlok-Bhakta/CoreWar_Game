8e58b6cb8f74055e3e7f3ec05b861168de3d567f
Shlok-Bhakta <62256489+Shlok-Bhakta@users.noreply.github.com>
2020-11-13 09:36:03 -0600

    added character movement still working on island rotation

---
+++ CoreWarUnityGame/Assets/MouseLook.cs
@@ -0,0 +1,46 @@
+﻿//https://www.youtube.com/watch?v=_QajrabyTJc thanks Brackeys for teaching me
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{   //creating a float to change the sensitivity
+    public float sensitivity = 300f;
+
+    //creating a float for the player body
+    public Transform playerBody;
+    
+    //creating a float for a camera
+    float xRotation = 0;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+ 
+    void Update()
+    {
+        //getting inputs for my mouse movement in the x and y axis
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        //applying sensitivity to the mouse movements and making it not move based on frame rate of the monitor
+        mouseX = mouseX * sensitivity * Time.deltaTime;
+        mouseY = mouseY * sensitivity * Time.deltaTime;
+
+        //getting movement of camera X axis
+
+        playerBody.Rotate(Vector3.up * mouseX);
+       
+        //getting camera movement on the y axis
+        xRotation -= mouseY;
+        
+        // clamping the camera so it doesnt spin to far
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+
+    }
+}
--- CoreWarUnityGame/Assets/islandrandomizer.cs
+++ CoreWarUnityGame/Assets/islandrandomizer.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class islandrandomizer : MonoBehaviour
 {
+
+    public float jankisland = -90f; 
     // Start is called before the first frame update
     void Start()
     {
+        
+        
+        
+        
         randommovement();
     }
     void randommovement()
     {
-       Quaternion randomMotiom = Quaternion.Euler(-90, 0, Random.Range(-180 , 180));
 
+        
+        Quaternion randomMotiom = Quaternion.Euler(jankisland, 0, Random.Range(0 , 360));
         transform.rotation = randomMotiom;
+
     }
     // Update is called once per frame
     void Update()
