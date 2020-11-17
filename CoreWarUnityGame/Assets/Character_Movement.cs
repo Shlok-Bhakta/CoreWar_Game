87fd794eb579d9cb7a9747a4b0839e01c1244e3b
Shlok-Bhakta <62256489+Shlok-Bhakta@users.noreply.github.com>
2020-11-17 13:50:00 -0600

    added character movement

---
+++ CoreWarUnityGame/Assets/Character_Movement.cs
@@ -0,0 +1,35 @@
+﻿//thanks again brakeys https://www.youtube.com/watch?v=_QajrabyTJc
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_Movement : MonoBehaviour
+{
+
+
+    //some other variables
+    public CharacterController Controller;
+    public float speed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        //creating variables for getting movement on an axis
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        //creating a vector3 for movement
+        Vector3 move = (transform.right * x) + (transform.forward * z);
+        
+        //applying the movement
+        Controller.Move(move * speed * Time.deltaTime);
+
+    }
+}
