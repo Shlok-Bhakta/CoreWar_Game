f3c5ee7bd0d380140621dde675a2d1c79eb52770
Shlok Bhakta <Shlok.Bhakta@ciscoisd.local>
2020-12-01 13:51:12 -0600

    added jank velocity

---
+++ CoreWarUnityGame/Assets/GravityforCharCont.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityforCharCont : MonoBehaviour
+{
+
+
+    //duh...
+    Vector3 Velocity;
+    //how fast we fall
+    public float GravityMultiplier = 9.81f;
+    //getting the character controller used for movement in Character_Movement.cs
+    public CharacterController playercont;
+
+
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        //adding the speed to velocity
+        Velocity.y += (GravityMultiplier * (-1)) * (Time.deltaTime * Time.deltaTime);
+        
+        //applying the velocity in the downward direction
+        playercont.Move(Velocity);
+    
+    
+    }
+}
