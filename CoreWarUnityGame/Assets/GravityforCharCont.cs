e671d978c1becea947abfbd98a7d9dc0e91a7455
Shlok Bhakta <Shlok.Bhakta@ciscoisd.local>
2020-12-02 13:51:01 -0600

    my attempt to fix the gravity and add jumping

--- CoreWarUnityGame/Assets/GravityforCharCont.cs
+++ CoreWarUnityGame/Assets/GravityforCharCont.cs
@@ -11,10 +11,19 @@ public class GravityforCharCont : MonoBehaviour
     //how fast we fall
     public float GravityMultiplier = 9.81f;
     //getting the character controller used for movement in Character_Movement.cs
-    public CharacterController playercont;
-
-
-
+    public CharacterController playerCont;
+    //setting a value for jump height
+    public float jumpHeight = 3f;
+    //checking for the ground - yeah.....
+    public Transform groundCheckL;
+    public Transform groundCheckR;
+    //generating a spere around the ground checker to fix the gravity
+    public float groundDist;
+    //What objects the sphere should check for
+    public LayerMask goundMask;
+    //if we are touching the ground isGround == True
+    bool isGroundL;
+    bool isGroundR;
 
     void Start()
     {
@@ -24,12 +33,37 @@ public class GravityforCharCont : MonoBehaviour
 
     void Update()
     {
+        //will check if a "sphere" around the point intersects with the ground
+        isGroundL = Physics.CheckSphere(groundCheckL.position, groundDist, goundMask);
+        isGroundR = Physics.CheckSphere(groundCheckR.position, groundDist, goundMask);
+
+        if(isGroundL && Velocity.y < 0)
+        {
+            Velocity.y = -2f;
+        }
+        if (isGroundR && Velocity.y < 0)
+        {
+            Velocity.y = -2f;
+        }
+
+
         //adding the speed to velocity
         Velocity.y += (GravityMultiplier * (-1)) * (Time.deltaTime * Time.deltaTime);
         
         //applying the velocity in the downward direction
-        playercont.Move(Velocity);
+        playerCont.Move(Velocity);
     
-    
+        //jumping as well
+
+        if(Input.GetButtonDown("Jump") && isGroundR)
+        {
+            //formula for gravity
+            Velocity.y = Mathf.Sqrt(jumpHeight * -2 * GravityMultiplier);
+        }
+
+
+
+
+
     }
 }
