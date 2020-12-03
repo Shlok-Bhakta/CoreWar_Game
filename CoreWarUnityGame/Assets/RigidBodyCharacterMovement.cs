0daf143569bc1e8ea506f722e5f403455e713ab9
Shlok Bhakta <Shlok.Bhakta@ciscoisd.local>
2020-12-03 13:50:33 -0600

    before making rigidbody movement

---
+++ CoreWarUnityGame/Assets/RigidBodyCharacterMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidBodyCharacterMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody character;
+    public Transform groundL;
+    public Transform groundR;
+    
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = GetComponent<Rigidbody>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Jump") && groundL)
+
+        }
+        
+
+    }
+}
